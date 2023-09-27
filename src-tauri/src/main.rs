b8b258451e03142a3780b46f656d3af6c7e940b7
Lucas Igarzabal <igarzaballucas@hotmail.com>
2023-09-27 11:16:23 -0300

    cambie sistema productos y proveedores para implementar relaciones para cuando sean bases de datos, falta checkear

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use core::panic;
+use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
+use std::io::{Read, Write};
 use std::sync::Mutex;
-use serde::de::DeserializeOwned;
-use std::{
-    collections::HashMap,
-    fs::OpenOptions,
-    io::{Read, Write},
-};
 use tauri::State;
 
 //---------------------------------Structs y Enums-------------------------------------
@@ -19,6 +15,13 @@ pub struct Sistema<'a> {
     proveedores: Vec<String>,
     path_prods: String,
     path_proveedores: String,
+    relaciones: Vec<Relacion>,
+}
+
+pub struct Relacion {
+    producto: Producto,
+    proveedor: String,
+    codigo: Option<u128>,
 }
 
 pub struct Venta<'a> {
@@ -31,7 +34,6 @@ pub struct Producto {
     //falta agregar el codigo de proveedor en vec, los proveedores en vec,
     //puede ser un hashmap o un vec de tuplas con referencia a una lista de proveedores
     //algunas cosas mas tambien como familia de productos
-    proveedores_codigos: HashMap<String, Option<u128>>,
     codigo_de_barras: u128,
     precio_de_venta: f64,
     porcentaje: f64,
@@ -42,12 +44,10 @@ pub struct Producto {
     cantidad: Presentacion,
 }
 
-pub struct Proveedor{
-    nombre:String,
-    contacto:u64,
+pub struct Proveedor {
+    nombre: String,
+    contacto: u64,
 }
-
-
 
 #[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
 pub enum Presentacion {
@@ -58,6 +58,15 @@ pub enum Presentacion {
 }
 
 //-----------------------------------Implementations---------------------------------
+impl Relacion {
+    pub fn new(producto: Producto, proveedor: String, codigo: Option<u128>) -> Self {
+        Relacion {
+            producto,
+            proveedor,
+            codigo,
+        }
+    }
+}
 
 impl<'a> Venta<'a> {
     pub fn new() -> Venta<'a> {
@@ -72,10 +81,11 @@ impl<'a> Sistema<'a> {
     pub fn new() -> Sistema<'a> {
         let path_prods = String::from("Productos.json");
         let path_proveedores = String::from("Proveedores.json");
-        let mut productos =Vec::new();
-        leer_file(&mut productos,path_prods.clone());
+        let mut productos = Vec::new();
+        leer_file(&mut productos, path_prods.clone());
         let mut proveedores = Vec::new();
-        leer_file(&mut proveedores,path_proveedores.clone());
+        leer_file(&mut proveedores, path_proveedores.clone());
+        let relaciones = Vec::new();
         println!(
             "A partir de aca estamos escribiendo los productos {:?}",
             productos
@@ -86,6 +96,7 @@ impl<'a> Sistema<'a> {
             proveedores,
             path_prods,
             path_proveedores,
+            relaciones,
         }
     }
     pub fn imprimir(&self) {
@@ -93,7 +104,8 @@ impl<'a> Sistema<'a> {
     }
     pub fn agregar(
         &mut self,
-        proveedores_codigos: HashMap<String, Option<u128>>,
+        proveedores: Vec<String>,
+        codigos_prov: Vec<String>,
         codigo_de_barras: &str,
         precio_de_venta: &str,
         porcentaje: &str,
@@ -104,8 +116,8 @@ impl<'a> Sistema<'a> {
         cantidad: &str,
         presentacion: &str,
     ) {
-        let prod = Producto::new(
-            proveedores_codigos,
+        let mut relacion = Vec::new();
+        let producto = Producto::new(
             codigo_de_barras,
             precio_de_venta,
             porcentaje,
@@ -116,7 +128,24 @@ impl<'a> Sistema<'a> {
             cantidad,
             presentacion,
         );
-        self.productos.push(prod);
+        if codigos_prov.len() > 0 {
+            for i in 0..proveedores.len() - 1 {
+                match codigos_prov[i].parse::<u128>() {
+                    Ok(a) => relacion.push(Relacion::new(
+                        producto.clone(),
+                        proveedores[i].clone(),
+                        Some(a),
+                    )),
+                    Err(_) => relacion.push(Relacion::new(
+                        producto.clone(),
+                        proveedores[i].clone(),
+                        None,
+                    )),
+                };
+            }
+        }
+        self.relaciones.extend(relacion);
+        self.productos.push(producto);
         match crear_file(self.path_prods.clone(), &self.productos) {
             Ok(_) => (),
             Err(e) => panic!("No se pudo porque {}", e),
@@ -132,7 +161,6 @@ impl Default for Presentacion {
 
 impl Producto {
     fn new(
-        proveedores_codigos: HashMap<String, Option<u128>>,
         codigo: &str,
         precio_de_venta: &str,
         porcentaje: &str,
@@ -150,7 +178,6 @@ impl Producto {
             _ => panic!("no posible"),
         };
         Producto {
-            proveedores_codigos,
             codigo_de_barras: codigo.parse().unwrap(),
             precio_de_venta: precio_de_venta.parse().unwrap(),
             porcentaje: porcentaje.parse().unwrap(),
@@ -182,12 +209,9 @@ fn crear_file<'a>(path: String, escritura: &impl Serialize) -> std::io::Result<(
     Ok(())
 }
 
-
-
-
 pub fn push(pr: Producto, path: String) {
     let mut prods = Vec::new();
-    leer_file(&mut prods,path.clone());
+    leer_file(&mut prods, path.clone());
     prods.push(pr);
     match crear_file(path.clone(), &prods) {
         Ok(_) => (),
@@ -195,13 +219,13 @@ pub fn push(pr: Producto, path: String) {
     };
 }
 
-fn leer_file<T:DeserializeOwned+Clone>(buf: &mut T,path: String){
+fn leer_file<T: DeserializeOwned + Clone>(buf: &mut T, path: String) {
     let file2 = File::open(path.clone());
     let mut file2 = match file2 {
         Ok(file) => file,
         Err(e) => panic!("{:?}", e),
     };
-    let mut buf2=String::new();
+    let mut buf2 = String::new();
     if let Err(e) = file2.read_to_string(&mut buf2) {
         panic!("No se pudo leer porque {}", e);
     }
@@ -211,10 +235,7 @@ fn leer_file<T:DeserializeOwned+Clone>(buf: &mut T,path: String){
             panic!("No se pudo porque {}", e)
         }
     }
-    
 }
-
-
 
 // -------------------------------Commands----------------------------------------------
 
@@ -244,19 +265,11 @@ fn agregar(
     cantidad: &str,
     presentacion: &str,
 ) -> String {
-    let mut res = HashMap::new();
     match sistema.lock() {
         Ok(mut sis) => {
-            if codigos_prov.len() > 0 {
-                for i in 0..proveedores.len() - 1 {
-                    match codigos_prov[i].parse::<u128>() {
-                        Ok(a) => res.insert(proveedores[i].clone(), Some(a)),
-                        Err(_) => res.insert(proveedores[i].clone(), None),
-                    };
-                }
-            }
             sis.agregar(
-                res,
+                proveedores,
+                codigos_prov,
                 codigo_de_barras,
                 precio_de_venta,
                 porcentaje,
