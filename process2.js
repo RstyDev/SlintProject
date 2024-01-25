2077c89fb3bec37c3f5ad951d6fb79bc6294b2f2
Lucas Igarzabal <igarzaballucas@hotmail.com>
2024-01-24 22:39:47 -0300

    guardando todo lo de afuera

--- process2.js
+++ process2.js
@@ -81,7 +81,7 @@ class Producto {
     }
 }
 
-let posact = 1462;
+let posact = 0;
 cantCodes = [1, 2, 3];
 marcas_chocolates = [
     "Hershey's",
@@ -167,87 +167,67 @@ marcas_galletitas = [
 ];
 variedades_galletitas = ['Chocolate', 'Surtidas', 'Vainilla', 'Limon', 'Glaseadas', 'Rellenas Chocolate']
 cantidades_galletitas = [150, 170, 300, 450, 600, 900];
-
+marcas_fideos= ["Barilla","Ronzoni","De Cecco","San Remo","La Molisana","Buitoni","Colavita","Giovanni Rana","Knorr","Catelli","Panzani",    "Misko",    "Ancient Harvest",    "Dreamfields",    "Mueller's",    "Hodgson Mill",    "Skinner",    "No Yolks",    "Annie Chun's", "Schar",];
+variedades_fideos=["Espaguetis","Fettuccine","Linguine","Penne","Rigatoni","Farfalle","Conchiglie","Rotini","Soba","Udon","Ramen","Capellini","Tagliatelle","Lasagna","Macarrones","Fideos de arroz","Somen","Pappardelle","Orzo","Cavatelli",];
+marcas_yerbas=["Amanda","La Tranquera","Rosamonte","Playadito","Taragüí","Union","Canarias","Nobleza Gaucha","Santo Pipo","Cruz de Malta","Aguantadora","Pajarito","Kraus","La Merced","Selecta","CBSe","Barão","Piporé","Yerba Mate Campesino","Mate Factor","Nativa","Sara","Simpli Mate","Verde Mate","La Potente","Mate Kurupí","Las Marías","Kurupi","Pipore","Coopetrabas","Del Cebador","Esperanza","Kraus Organic","Cruz de Malta Tereré","Sol de Acuario","Campesino Orgánico","Yacuy","Biofresco","Cachamate","Yerba Pajarito Selva","Mañanita","BIO Yerba Mate","Rosamonte Especial","Cruceña","Kerbal",];
+variedades_yerbas=["Clásica", "Suave", "Sin Palo", "Bajo Contenido de Polvo"];
+cantidades_yerbas=[250,500,1000];
+marcas_mermeladas=["Smucker's","Bonne Maman","Stonewall Kitchen","Welch's","Hero","Sarabeth's","Tiptree","Bama","Polaner","Mrs. Bridges","Wilkin & Sons","Dickinson's","Frog Hollow Farm","Crofters","Bonne Maman","Fruitfield","Duerr's","Hartley's","Beerenberg","Rigoni di Asiago","St. Dalfour","Braswell's","Buderim Ginger","Folláin","Bonne Maman","Welch's","Tiptree","Bon Maman","Mackays","L'arbre Vert","Bar-le-Duc","Bionaturae","Clearspring","Dalfour","D'arbo",  ];
+variedades_mermeladas=["Fresa","Frambuesa","Arándano","Ciruela","Mora","Melocotón","Naranja","Mandarina","Piña","Manzana","Cereza","Albaricoque","Higo","Uva","Kiwi","Pomelo","Limón","Jengibre","Coco","Arándano rojo","Mango","Pera","Granada","Melón","Frutas del bosque","Tomate","Papaya","Canela","Vainilla","Rosa","Pimiento","Elderberry","Zanahoria",];
+marcas_harinas=["Gold Medal","King Arthur Flour","Bob's Red Mill","Pillsbury","Robin Hood","White Lily","Heckers","Arrowhead Mills","Caputo","Antimo Caputo","Great River Organic Milling","Central Milling","Hodgson Mill","Goya","King Milling","Organic Gemini","Jovial","Namaste Foods","Authentic Foods","Bobs Well Flour","Honeyville","Azure Market Organics","Anna","Martha White","Doves Farm","Red Star","Pamela's Products","Krusteaz","Quaker","Admiral","Bacheldre Mill","Breadtopia","BRM","Ceresota","Community Grains","ConAgra Mills","Cup4Cup","Dietz & Watson",];
+variedades_harinas=["Harina 000","Harina 0000","Harina Integral","Harina de Maíz","Harina Leudante","Harina de Garbanzo","Harina de Arroz","Harina de Maíz Precocida","Harina de Soja","Harina de Almendra",];
+marcas_panes=["Bimbo","Lactal","Fargo","Bagley","San Cayetano","Grandiet","Calsa","Grupo Los Grobo","Forcela","Vea","Wentz","Faverin","Nutrella","Calsa","2 de Oro","Oki","Vizzolini","Argentinos","Morella","La Tradicional","La Salteña","Lory","Don Satur","Tante María","Viguisa",];
+variedades_panes=["Blanco","Integral","de Centeno","Multicereal","de Salvado","de Soja","de Avena","de Espelta","de Maíz","de Semillas","de Nueces","de Pasas","de Aceitunas","de Queso","de Pita","Baguette","Ciabatta","de Molde","de Linaza","de Chía","de Kamut","de Quinoa","Brioche","Challah","Naan","Focaccia","Lavash","de Centeno Integral","sin Gluten","de Calabacín","de Zanahoria","de Tomate Seco","de Remolacha","de Cerveza",];
+cantidades_panes=[350,400,500,600];
+marcas_gaseosas = ["Coca-Cola","Pepsi","Sprite","Fanta","Dr Pepper","7UP","Mountain Dew","Canada Dry","Schweppes","Crush","Sunkist","A&W Root Beer","Barq's","Mirinda","RC Cola","Big Red","Cheerwine","Squirt","Jarritos","Inca Kola"];
+variedades_gaseosas = ["Cola","Lima-limón","Naranja","Uva","Fresa","Manzana","Cereza","Frambuesa","Piña","Jengibre","Toronja","Limón","Tónica","Mango","Melocotón","Cítricos mixtos","Frutas tropicales","Vainilla","Root Beer","Kiwi"];
+cantidades_gaseosas_ml = [354, 500];
+cantidades_gaseosas_lt = [1.5,2.25,3];
 
 let prods = [];
 function procesar_datos(tipo_producto, marcas, variedades, cantidades, presentacion) {
     let cant_tot = marcas.length * variedades.length * cantidades.length;
     for (let i = 0; i < cant_tot; i++) {
-        let esta=false;
-        let codigo;
-        let marca;
-        let variedad;
-        let cantidad;
-        do{
-            codigo=getCode();
-            for (let j=0;j<prods.length;j++){
-                if(prods[j].codigos_de_barras.includes(codigo)){
-                    esta=true;
-                }
-            }
-        }while(esta)
-        
-        do{
-            marca=elegirAleatoriamente(marcas);
-            variedad=elegirAleatoriamente(variedades);
-            cantidad=elegirAleatoriamente(cantidades);
-            for (let j=0;j<prods.length;j++){
-                switch (Object.keys(prods[j].presentacion)){
-                    case 'Gr':
-                        if(marca==prods[j].marca&&variedad==prods[j].variedad&&cantidad==prods[j].presentacion.Gr==cantidad){
-                            esta=true;
-                        };
-                        break;
-                    case 'Un':
-                        if(marca==prods[j].marca&&variedad==prods[j].variedad&&cantidad==prods[j].presentacion.Un==cantidad){
-                            esta=true;
-                        };
-                        break;
-                    case 'Lt':
-                        if(marca==prods[j].marca&&variedad==prods[j].variedad&&cantidad==prods[j].presentacion.Lt==cantidad){
-                            esta=true;
-                        };
-                        break;
-                    case 'Ml':
-                        if(marca==prods[j].marca&&variedad==prods[j].variedad&&cantidad==prods[j].presentacion.Ml==cantidad){
-                            esta=true;
-                        };
-                        break;
-                    case 'CC':
-                        if(marca==prods[j].marca&&variedad==prods[j].variedad&&cantidad==prods[j].presentacion.CC==cantidad){
-                            esta=true;
-                        };
-                        break;
-                    case 'Kg':
-                        if(marca==prods[j].marca&&variedad==prods[j].variedad&&cantidad==prods[j].presentacion.Kg==cantidad){
-                            esta=true;
-                        };
-                        break;                        
-                }
-                
-            }
-        }while(esta)
-
         let costo = getCostPr();
         let codigos = [];
         for (let j = 0; j < elegirAleatoriamente(cantCodes); j++) {
-            codigos.push(codigo);
+            let code;
+            let esta=true;
+            while (esta){
+                esta=false;
+                code=getCode();
+                for (let h=0;h<prods.length;h++){
+                    if (prods[h].codigos_de_barras.includes(code))
+                    esta=true;
+                }
+            }
+            codigos.push(getCode());
         }
-        let prod_act = new Producto(posact, codigos, getSalePr(costo), costo, tipo_producto, marca, variedad, presentacion, cantidad);
+        let prod_act = new Producto(posact, codigos, getSalePr(costo), costo, tipo_producto, elegirAleatoriamente(marcas), elegirAleatoriamente(variedades), presentacion, elegirAleatoriamente(cantidades));
+        let esta=false;
         for (let j=0;j<prods.length;j++){
-            if (prods[j].marca==prod_act.marca&&prod_act.presentacion&&prods[j].presentacion&&prod_act.variedad==prods[j].variedad){
-                
-            }        
+            if (prod_act.marca==prods[j].marca&&prod_act.variedad==prods[j].variedad&&prod_act.presentacion==prods[j].presentacion){
+                esta=true
+            }
         }
+        if(!esta)
         prods.push(prod_act)
+        posact++;
     }
 }
 procesar_datos('Chocolate', marcas_chocolates, variedades_chocolate, cantidades_chocolates, 'Gr');
 procesar_datos('Arroz',marcas_arroz,variedades_arroz,cantidades_arroz,'Gr');
 procesar_datos('Galletitas',marcas_galletitas,variedades_galletitas,cantidades_galletitas,'Gr');
+procesar_datos('Fideos',marcas_fideos,variedades_fideos,[500],'Gr');
+procesar_datos('Yerba', marcas_yerbas,variedades_yerbas,cantidades_yerbas,'Gr');
+procesar_datos('Mermelada',marcas_mermeladas,variedades_mermeladas,[400],'Gr');
+procesar_datos('Harina',marcas_harinas,variedades_harinas,[1],'Kg');
+procesar_datos('Pan De Mesa',marcas_panes,variedades_panes,cantidades_panes,'Gr');
+procesar_datos('Gaseosa',marcas_gaseosas,variedades_gaseosas,cantidades_gaseosas_ml,'Ml');
+procesar_datos('Gaseosa', marcas_gaseosas, variedades_gaseosas, cantidades_gaseosas_lt, 'Lt');
 
-console.log(prods);
 
+console.log(JSON.stringify(prods));
 
 let algo3 = {
     "id": 1462,
