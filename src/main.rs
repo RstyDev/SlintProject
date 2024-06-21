20e96e2dd015f12e694ebc339c07516f408d6600
Lucas Igarzabal <igarzaballucas@hotmail.com>
2024-06-21 19:49:11 -0300

    por fin resolvi como usar bien el global, esta genial

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,16 @@ slint::include_modules!();
 mod db;
 mod mods;
 
+fn set_logic(log: Logic){
+    log.on_pagar(|a,b|{println!("{} {}",a,b); a+b});
+    log.on_test(|dato|{println!("{dato}")});
+}
+
+
 fn main()->Result<(),slint::PlatformError> {
     let ui = AppWindow::new()?;
     let ui_handle = ui.as_weak();
-    let res=ui.global::<Logic>();
+    set_logic(ui.global::<Logic>());
     ui.on_request_increase_value(move || {
         let ui= ui_handle.unwrap();
         ui.set_counter(ui.get_counter() + 1);
